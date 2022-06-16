b9e51b8c07d9c123c4fffa536b8869d309e7750c
LSK1102 <c8803977@gmail.com>
2022-06-16 20:04:09 +0800

    Add

---
+++ wed410928343/Controllers/ExamController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wed410928343.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: Exam
+        public ActionResult About(float? A)
+        {
+            var result = "";
+            if (A==1) {
+                result = "星期一";
+            }
+            if (A == 2)
+            {
+                result = "星期二";
+            }
+            if (A == 3)
+            {
+                result = "星期三";
+            }
+            if (A == 4)
+            {
+                result = "星期四";
+            }
+            if (A == 5)
+            {
+                result = "星期五";
+            }
+            if (A == 6)
+            {
+                result = "星期六";
+            }
+            if (A == 7)
+            {
+                result = "星期日";
+            }
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
---
+++ wed410928343/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="wed410928343.MvcApplication" Language="C#" %>
